9d6cf627302a7a29ed9cea96f7d7921bc8103231
Mark Hill <markleehill@gmail.com>
2017-09-13 21:57:47 -0700

    archived old scripts and moved scripts out of subdirectories

--- scripts/getCubesatPic.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-ssh cubesatgateway "scp flt-irvine-01:/root/snap.-Z.0.jpg ."
-scp cubesatgateway:/home/avionics/snap.-Z.0.jpg .
--- scripts/loginsudocheck.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [ "$(ls /home/public/sudoqueue | grep sudoqueue | wc -l )" -ne "0" ]
-	then
-		printf "Items found in sudo queue\n\n"
-		sudo sudoscript
-	else
-		exit 0 #printf "No items in sudo queue\n"
-fi
