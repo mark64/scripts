86752a125c8d99cc50a290830d091bdb2991830b
Mark Hill <markleehill@gmail.com>
2018-12-03 20:54:49 -0800

    remove unnecessary scripts

--- cs61c-grade-check.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-ssh inst "source ~/.bash_profile; glookup"
--- parition_backup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ $# -ne 1 ]; then
-	printf "need the device name"
-	exit 1
-fi
-
-SDCARD=$1
-dd if=/dev/$SDCARD bs=1M conv=fsync status=progress | gzip > $SDCARD.img.gz
